95a6f0994688eb5d4001b24afebcbdfd5ca5e27d
Christopher Welborn <cj@welbornprod.com>
2019-07-07 14:18:34 -0500

    Add .fzf source files.
    Sublime (main/linter) config changed recently.

---
+++ .fzf.zsh
@@ -0,0 +1,14 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/cj/clones/fzf/bin* ]]; then
+  export PATH="$PATH:/home/cj/clones/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/cj/clones/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/cj/clones/fzf/shell/key-bindings.zsh"
+
